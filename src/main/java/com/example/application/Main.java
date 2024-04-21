2fd646646b061c0f1cff7bf25d52b7849ebb927a
jgbinegar <jgbinegar@gmail.com>
2024-04-21 18:51:10 -0400

    Jar Built

---
+++ src/main/java/com/example/application/Main.java
@@ -0,0 +1,9 @@
+package com.example.application;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
